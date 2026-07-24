fd88d5a620dc52be5d35f437c4e0ed89e950e639
okparade <rhuonabazunu@gmail.com>
2026-07-24 15:32:43 -0700

    Update backend API URL to Render

--- js/api-config.js
+++ js/api-config.js
@@ -7,4 +7,4 @@
 const API_BASE_URL =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? 'http://localhost:5000'
-    : 'https://yourusername.pythonanywhere.com';
+    : 'https://attendpro-i7n8.onrender.com';
